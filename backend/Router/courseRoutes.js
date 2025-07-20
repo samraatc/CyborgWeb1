a87c0cfc7a3659a796d7ac126af26dfd75973355
Bishnu Chaudhary <bishnuoff0031@gmail.com>
2025-07-20 23:26:49 +0530

    error

--- backend/Router/courseRoutes.js
+++ backend/Router/courseRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Course = require('../Model/course');
+const Course = require('../Model/Course');
 
 // POST a new course
 router.post('/', async (req, res) => {
